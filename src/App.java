065a254ecc5d6a31f3b4ae82e13ad6202427fbd5
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2021-12-01 07:34:47 -0300

    Update
    Update

---
+++ src/App.java
@@ -0,0 +1,13 @@
+class SnitSoft {
+
+    public static void main(String args[]) {
+        double valor;
+
+        valor = 5.95;
+        valor = valor + 25.00;
+
+        System.out.print("Você irá pagar $");
+        System.out.print(valor);
+        System.out.println(" para seu cartão de crédito.");
+    }
+}
